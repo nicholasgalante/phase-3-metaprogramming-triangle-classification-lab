2ed9a6c0104cafe1b2f161bd1b0b9176da63c540
Nicholas Galante <nicholasgalante94@gmail.com>
2023-02-24 00:17:55 -0500

    commit tree 7fe3e1248672f8d150a838a9ffb26fc869c329cb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    raise TriangleError unless validate_length? && validate_tri_ineq? 
+    if (a == b && b == c)
+      :equilateral
+    elsif (a == b || b == c || a == c)
+      :isosceles
+    elsif (a != b && b != c && a != c)
+      :scalene
+    end
+  end
+
+  def validate_length?
+    a.positive? && b.positive? && c.positive?
+  end
+
+  def validate_tri_ineq?
+    a + b > c && b + c > a && a + c > b
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid lengths. The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. This is a principle known as the triangle inequality."
+    end
+  end
+
 end
